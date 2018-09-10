97351dc23dfbb97b02ba42361b8ab96c7a6bdcd4
xiaomo <mojiale66@163.com>
2018-09-10 19:16:24 +0800

    init project

---
+++ src/main/java/com/gosuncn/netty/GoNettyComponentApplication.java
@@ -0,0 +1,12 @@
+package com.gosuncn.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoNettyComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoNettyComponentApplication.class, args);
+	}
+}
